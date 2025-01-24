d7169d4b60fcb581eaf55326fc854600d4d40888
annaannaA33 <annaansmva@gmail.com>
2025-01-24 15:39:11 +0200

    feat: add fallback URL for GIFs in case of API error

--- src/services/messageService.ts
+++ src/services/messageService.ts
@@ -1,9 +1,11 @@
 import { db } from "../models/db";
+import { sql } from "kysely";
 
 export const getRandomTemplate = async (): Promise<string> => {
     const templates = await db
         .selectFrom("templates")
         .selectAll()
+        .orderBy(sql`RANDOM()`)
         .limit(1)
         .execute();
     if (templates.length === 0) {
--- src/utils/giphy.ts
+++ src/utils/giphy.ts
@@ -1,29 +1,38 @@
 import axios from "axios";
-import { config } from "dotenv"
+import { config } from "dotenv";
 
 config();
+
+if (!process.env.GIPHY_API_KEY) {
+    throw new Error("GIPHY_API_KEY is not defined in .env file.");
+}
+if (!process.env.FALLBACK_GIF_URL) {
+    throw new Error("FALLBACK_GIF_URL is not defined in .env file.");
+}
+
 const GIPHY_API_KEY = process.env.GIPHY_API_KEY;
+const FALLBACK_GIF_URL = process.env.FALLBACK_GIF_URL;
 
 export const getRandomGif = async (): Promise<string> => {
     try {
         const response = await axios.get(
             `https://api.giphy.com/v1/gifs/random?api_key=${GIPHY_API_KEY}&tag=celebration&rating=g`
         );
-        // Проверяем, есть ли URL в ответе
-        //const gifUrl = response.data.data?.[0]?.url;
         const gifUrl = response.data.data?.url;
 
         if (!gifUrl) {
             console.warn("No GIF URL found in Giphy API response");
-            return "";
+            return FALLBACK_GIF_URL;
         }
 
         return gifUrl;
     } catch (error) {
-        console.error(
-            "Error fetching GIF:",
-            error.response?.data || error.message
-        );
-        throw new Error("Unable to fetch GIF");
+        if (error instanceof Error) {
+            console.error("Error fetching GIF:", error.message);
+        } else {
+            console.error("An unexpected error occurred:", error);
+        }
+        console.warn("Using fallback GIF.");
+        return FALLBACK_GIF_URL;
     }
 };
